db598d03206c02368544ee7bb9aff5870fbaaa90
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2023-02-09 14:39:50 +0700

    Create main.java
    Entry point of the application.

---
+++ main.java
@@ -0,0 +1,19 @@
+package com.application.compbuilder;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 650);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
